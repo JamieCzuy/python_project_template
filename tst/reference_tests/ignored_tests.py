5d4510e8b463545356d959a36415cc9447b18beb
Jamie Czuy <jamie.czuy@ey.com>
2015-12-20 10:30:58 -0500

    Add examples of ignored tests

---
+++ tst/reference_tests/ignored_tests.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest import TestCase
+
+try:
+    import fictitious_module
+except ImportError:
+    fictitious_module = None
+
+class TC(TestCase):
+
+    @unittest.skip('This test is always skipped (ignored)')
+    def test01_a_test_that_is_ignored(self):
+        self.fail('This test is ignored (skipped) - does not fail!')
+
+    @unittest.skipIf(fictitious_module is None, 'This test is skipped because fictitious module is not imported')
+    def test02_a_test_that_is_conditionally_ignored(self):
+        self.fail('This test is ignored (skipped) - does not fail!')
+
+
