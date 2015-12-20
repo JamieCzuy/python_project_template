7643dff99ee806cb3fb4cef852b890a3d53daad9
Jamie Czuy <jamie.czuy@ey.com>
2015-12-20 10:43:03 -0500

    Add ignoring lines to check coverage

--- tst/reference_tests/ignored_tests.py
+++ tst/reference_tests/ignored_tests.py
@@ -10,10 +10,10 @@ class TC(TestCase):
 
     @unittest.skip('This test is always skipped (ignored)')
     def test01_a_test_that_is_ignored(self):
-        self.fail('This test is ignored (skipped) - does not fail!')
+        self.fail('This test is ignored (skipped) - does not fail!')    # ignore during coverage check
 
     @unittest.skipIf(fictitious_module is None, 'This test is skipped because fictitious module is not imported')
     def test02_a_test_that_is_conditionally_ignored(self):
-        self.fail('This test is ignored (skipped) - does not fail!')
+        self.fail('This test is ignored (skipped) - does not fail!')    # ignore during coverage check
 
 
